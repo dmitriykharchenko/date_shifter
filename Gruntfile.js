d99809f042a39fd9dc389f8f9f932a0f2af0ddbb
Dmitriy Kharchenko <akimoto@yandex.ru>
2013-12-01 19:54:34 +0700

    grunt for building scripts

---
+++ Gruntfile.js
@@ -0,0 +1,77 @@
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    // jasmine: {
+    //   unit: {
+    //     src: "dist/date_shifter.js",
+    //     options: {
+    //       keepRunner: true,
+    //       specs: 'spec/*_spec.js',
+    //       // vendor: 'spec/vendor/*.js'
+    //       helpers: ['spec/**/**_fixture.js']
+    //     }
+    //   }
+    // },
+
+    clean: {
+      tmp: ['_tmp']
+    },
+
+    uglify: {
+      javascripts: {
+        files: {
+          'dist/date_shifter.min.js': [
+            'dist/date_shifter.js'
+          ]
+        }
+      }
+    },
+
+    coffee: {
+      compile: {
+        files: {
+          'dist/date_shifter.js': 'src/date_shifter.coffee'
+        },
+        options: {
+          bare: true
+        }
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: ['src/date_shifter.coffee', 'spec/**/**.js'],
+        tasks: ['compile'],
+        options: {
+          atBegin: true
+        }
+      }
+    },
+
+    jshint: {
+      all: 'dist/date_shifter.js',
+      options: {
+        reporter: 'jslint',
+        reporterOutput: '../javascriptslint.xml'
+      }
+    }
+
+  });
+
+  grunt.event.on('watch', function(action, filepath, target) {
+    grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
+  });
+
+  grunt.registerTask('compile', ['clean', 'coffee']);
+  grunt.registerTask('build', ['compile', 'uglify']);
+  grunt.registerTask('test', ['compile', 'jasmine:unit']);
+
+};
---
+++ dist/date_shifter.js
@@ -0,0 +1,49 @@
+var date_shifter;
+
+date_shifter = new function() {
+  var date_methods, shift_date;
+  date_methods = {
+    "year": "FullYear",
+    "month": "Month",
+    "date": "Date"
+  };
+  shift_date = function(date, quantity_ops, is_clean) {
+    var method_name, name, quantity, value;
+    for (name in quantity_ops) {
+      quantity = quantity_ops[name];
+      method_name = date_methods[name];
+      value = date["get" + method_name]() + quantity;
+      date = new Date(date["set" + method_name](value));
+    }
+    if (is_clean) {
+      return new Date(date.setHours(0, 0, 0, 0));
+    } else {
+      return date;
+    }
+  };
+  return {
+    hours: function(date, quantity) {
+      return shift_date(new date, {
+        hour: quantity
+      });
+    },
+    days: function(date, quantity, is_clean) {
+      return shift_date(new Date(date), {
+        date: quantity
+      }, is_clean);
+    },
+    weeks: function(date, quantity, is_clean) {
+      return this.days(date, quantity * 7, is_clean);
+    },
+    months: function(date, quantity, is_clean) {
+      return shift_date(new Date(date), {
+        month: quantity
+      }, is_clean);
+    },
+    years: function(date, quantity, is_clean) {
+      return shift_date(new Date(date), {
+        year: quantity
+      }, is_clean);
+    }
+  };
+};
